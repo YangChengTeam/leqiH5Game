c9deb94c35d32171090446178995fc40c69bd9b6
mzpbvsig <972403325@qq.com>
2018-06-26 08:22:03 +0800

    full it

--- leqiH5Game/ViewController.m
+++ leqiH5Game/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+
+#import <sys/utsname.h>
+#import <AdSupport/AdSupport.h>
+
 #import "GameWebView.h"
 #import "Common.h"
 
@@ -14,6 +18,8 @@
 
 @property (nonatomic, copy) NSString *gameUrl;
 @property (nonatomic, strong) GameWebView *gameWebView;
+@property (nonatomic, weak) IBOutlet UIImageView *gameImageView;
+@property (nonatomic, weak) IBOutlet UITextField *gameProcessTextField;
 
 @end
 
@@ -25,14 +31,65 @@
     _gameUrl = GAME_URL;
     _gameWebView = [[GameWebView alloc] init];
     _gameWebView.navigationDelegate = self;
+    _gameWebView.customUserAgent = [NSString stringWithFormat:@"iOS-%@", [self getSysytemInfo]];
+    [_gameWebView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
     [self.view addSubview:_gameWebView];
     
+    
     [_gameWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_gameUrl]]];
+    
+    [self.view bringSubviewToFront:_gameImageView];
+    [self.view bringSubviewToFront:_gameProcessTextField];
+}
+
+
+- (NSString *)getSysytemInfo {
+    UIDevice *device = [UIDevice currentDevice];
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    NSString *model = [NSString stringWithCString:systemInfo.machine
+                                         encoding:NSUTF8StringEncoding];
+
+    NSString *idfa = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+    idfa = [[idfa stringByReplacingOccurrencesOfString:@"-"
+                                            withString:@""] lowercaseString];
+    return [NSString stringWithFormat:@"%@-%@-%@-%@",
+            idfa,
+            device.name,
+            model,
+            device.systemVersion
+            ];
 }
 
 - (void)viewDidLayoutSubviews {
     _gameWebView.frame = self.view.bounds;
+    _gameImageView.frame = self.view.bounds;
 }
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+     [_gameWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:_gameUrl]]];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        _gameProcessTextField.text = [NSString stringWithFormat:@"加载%d%%...",
+                                      (int)(_gameWebView.estimatedProgress * 100)];
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    _gameProcessTextField.text = @"加载完成";
+    _gameProcessTextField.hidden = YES;
+    _gameImageView.hidden = YES;
+}
+
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
     
@@ -43,7 +100,6 @@
 }
 
 - (void)pay:(NSString *)urlStr withUrl:(NSURL *)url {
-    
     NSLog(@"%@", urlStr);
     if ([urlStr containsString:@"alipay://"] || [urlStr containsString:@"weixin://"]) {
         [[UIApplication sharedApplication] openURL:url];
